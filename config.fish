b8b137214f97276e3327ed9d803b85a46fc74e02
vvebninja <aleksandru.morar@gmail.com>
2026-01-27 17:51:08 +0200

    greeting.fn

--- config.fish
+++ config.fish
@@ -1,24 +1,29 @@
+set -g fish_greeting ""
+
+
 if not functions -q fisher
     echo "Installing Fisher..."
     curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
     fisher update
 end
 
-
 if status is-interactive
-  alias n 'nvim'
-  alias ndir 'cd ~/.config/nvim/'
-  alias p 'pnpm'
+  alias n     'nvim'
+  alias ndir  'cd ~/.config/nvim/'
+  alias p     'pnpm'
+
+  alias nrd    'npm run dev'
+  alias ni    'npm i'
   
   alias conff 'nvim ~/.config/fish/config.fish'
-  alias srcf 'source ~/.config/fish/config.fish'
+  alias srcf  'source ~/.config/fish/config.fish'
 
-  alias dnfi 'sudo dnf install -y'
-  alias dnfr 'sudo dnf remove -y'
-  alias dnfu 'sudo dnf upgrade'
+  alias dnfi  'sudo dnf install -y'
+  alias dnfr  'sudo dnf remove -y'
+  alias dnfu  'sudo dnf upgrade -y'
 
-  alias gaac 'git add . && git commit -m'
-  alias gp   'git push'
+  alias gp    'git push'
+  alias gst   'git status'
 end
 
 # pnpm
---
+++ functions/greeting.fish
@@ -0,0 +1,3 @@
+function fish_greeting
+    echo (set_color cyan)"󰈺 󰚀 󱑎" (date +%H:%M:%S)
+end
